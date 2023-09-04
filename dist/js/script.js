ae7605d96d24b4d7adbe3bf7b393e7ecb8a68907
trueSlav <2001idercach@gmail.com>
2023-09-04 16:29:30 +0300

    A scroll effect page

---
+++ dist/js/script.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+	gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+	//Плавный скролл
+	if (ScrollTrigger.isTouch !== 1) {
+		
+		ScrollSmoother.create({
+			wrapper: '.wrapper',
+			content: '.content',
+			smooth: 1.5,
+			effects: true
+		});
+
+		gsap.fromTo('.hero-section', {opacity: 1}, {
+			opacity: 0,
+			scrollTrigger: {
+				trigger: '.hero-section',
+				start: 'center',
+				end: '850',
+				scrub: true
+			}
+		});
+
+		//Анимация галереи
+
+		let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+		itemsL.forEach(item => {
+			gsap.fromTo(item, {x: -200, opacity: 0}, {
+				opacity: 1, x: 0,
+				scrollTrigger: {
+					trigger: item,
+					start: -850,
+					scrub: true
+				}
+			})
+		})
+
+		let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+		itemsR.forEach(item => {
+			gsap.fromTo(item, {x: 200, opacity: 0}, {
+				opacity: 1, x: 0,
+				scrollTrigger: {
+					trigger: item,
+					start: -850,
+					scrub: true
+				}
+			})
+		})
+	}
+
+});
